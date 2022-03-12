c7664cf81c9979d57bd17fd0c10f35d283b5dac1
Si3108 <s31082604@gmail.com>
2022-03-12 22:11:47 +0800

    First Homework

---
+++ shapes.h
@@ -0,0 +1,53 @@
+#ifndef _SHAPES_H_
+#define _SHAPES_H_
+
+#define PI 3.1415926
+
+// Base class
+class Shape {
+public:
+	Shape(double a = 0, double b = 0) {
+		width = a; height = b;
+	}
+	~Shape() {}
+
+	void setWidth(double w) {
+		width = w;
+	}
+	void setHeight(double h) {
+		height = h;
+	}
+	virtual double area() = 0;
+
+protected:
+	double width;
+	double height;
+};
+
+class Rectangle : public Shape {
+public:
+	Rectangle(double a = 0, double b = 0) :Shape(a, b) { }
+
+	double area() {
+		return (width * height);
+	}
+};
+
+class triangle : public Shape {
+public:
+	triangle(double a = 0, double b = 0) :Shape(a, b) { }
+
+	double area() {
+		return (width * height * 0.5);
+	}
+};
+
+class circle : public Shape {
+public:
+	circle(double a = 0, double b = 0) :Shape(a, b) { }
+
+	double area() {
+		return (width * PI * width);
+	}
+};
+#endif
